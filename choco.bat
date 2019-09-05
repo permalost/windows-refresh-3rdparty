c35df434c6a7cfca22f43d282eaa70e4aef1428f
permalost <permalost@gmail.com>
2019-09-05 09:50:27 -0600

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -13,6 +13,12 @@ choco install -y git
 choco install -y kubernetes-helm
 choco install -y kubectx-ps
 choco install -y vscode
+choco install -y vscode-powershell
+choco install -y vscode-docker
+choco install -y vscode-eslint
+choco install -y vscode-settingssync
+choco install -y vscode-yaml
+choco install -y openjdk
 
 :: Games
 choco install -y steam

6d41af5e88611c86cecedc1b94c45e5400f4413f
permalost <permalost@gmail.com>
2022-10-08 13:56:14 -0600

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -4,36 +4,64 @@
 
 :: -y confirm yes for any prompt during the install process 
 
+:: Security
+choco install -y malwarebytes
+choco install -y bitwarden
+choco install -y bitdefenderavfree
+:: - VPN
+choco install -y tailscale
+
 :: Basics
 choco install -y googlechrome
 
 :: Dev work
-choco install -y docker-desktop
+:: - Basic
 choco install -y git
-choco install -y kubernetes-helm
-choco install -y kubectx-ps
+:: choco install -y putty.install
+choco install -y make
+:: - Languages
+choco install -y openjdk
+choco install -y python
+choco install -y nvm
+:: - IDEs
+choco install -y intellijidea-ultimate
+choco isntall -y arduino
 choco install -y vscode
+choco install -y vscode-arduino
 choco install -y vscode-powershell
 choco install -y vscode-docker
 choco install -y vscode-eslint
 choco install -y vscode-settingssync
 choco install -y vscode-yaml
 choco install -y vscode-spring-initializr
-choco install -y openjdk
-choco install -y putty.install
-:: choco install nvidia-display-driver
-:: choco install awscli
-:: choco install wireshark
+:: - Docker/k8s
+choco install -y docker-desktop
+choco install -y kubernetes-helm
+:: choco install -y kubectx-ps
+:: - Other
+:: choco install -y awscli
+:: choco install -y wireshark
 
 :: Games
 choco install -y steam
 choco install -y epicgameslauncher
 
+:: Projects
+choco install -y inkscape
+
+
 :: Entertainment
 choco install -y vlc
 
 :: Other
-choco install -y bitwarden
+:: - Filesharing
 choco install -y dropbox
+choco install -y nextcloud-client
+:: - misc
 choco install -y f.lux
 choco install -y windirstat
+choco install -y nvidia-display-driver
+choco install -y logitech-webcam-software
+choco install -y lghub
+choco install -y logitechgaming
+:: choco install -y zoom
